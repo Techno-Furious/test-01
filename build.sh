06549be5d5162428bdb5a5e87cc95fb3c49de54e
Nuthan B <156648630+Techno-Furious@users.noreply.github.com>
2024-12-10 19:17:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 # Install Playwright and its dependencies
 pip install -r requirements.txt
 playwright install --with-deps
+sudo apt update
+sudo apt install -y wget unzip
+sudo apt install -y chromium-browser chromium-chromedriver
